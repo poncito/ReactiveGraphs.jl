01ec5732a8d694bb1826cbdc7fec86993c3f8cc5
Romain Poncet <romain.pct@gmail.com>
2023-04-19 09:21:16 +0100

    foldl

--- src/foldl.jl
+++ src/foldl.jl
@@ -1,39 +1,33 @@
-mutable struct Map{T,F} <: Operation{T}
+mutable struct Foldl{TState,F} <: Operation{TState}
     f::F
-    x::T
-    Map{T}(f::F) where {T,F} = new{T,F}(f)
+    state::TState
 end
+@inline getvalue(x::Foldl) = x.state
+@inline getvalue(::ListNode, element::Foldl) = getvalue(element)
 
-@inline getvalue(x::Map) = x.x
-@inline getvalue(::ListNode, element::Map) = getvalue(element)
-
-@inline function update!(m::Map, args...)
-    @inline m.x = m.f(args...)
+@inline function update!(m::Foldl, args...)
+    @inline m.state = m.f(m.state, args...)
     nothing
 end
 
-function Base.map(
+function Base.foldl(
     f::Function,
+    state::TState,
     arg::Node,
     args::Node...;
     name::Union{Nothing,Symbol} = nothing,
-)
-    uniquename = genname(name)
-    argtypes = getoperationtype.((arg, args...))
-    T = Base._return_type(f, Tuple{argtypes...})
-    op = Map{T}(f)
-    Node(uniquename, op, arg, args...)
+) where {TState}
+    Node(genname(name), Foldl(f, state), arg, args...)
 end
 
-
-function generate(::Symbol, name::Symbol, parentnames::NTuple{<:Any,Symbol}, ::Type{<:Map})
+function generate(::Symbol, name::Symbol, parentnames::NTuple{<:Any,Symbol}, ::Type{<:Foldl})
     updated_s = Symbol(:updated, name)
     initialized_s = Symbol(:initialized, name)
     nodename_s = Meta.quot(name)
     args = (:(getvalue(list, $(Meta.quot(n)))) for n in parentnames)
     condition_updated = Expr(:call, :|, (Symbol(:updated, n) for n in parentnames)...)
     condition_initialized =
-        Expr(:call, :&, (Symbol(:initialized, n) for n in parentnames)...)
+    Expr(:call, :&, (Symbol(:initialized, n) for n in parentnames)...)
     quote
         $updated_s = if $condition_initialized & $condition_updated
             node = getnode(list, $nodename_s)
